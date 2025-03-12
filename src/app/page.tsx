604eee81bcd0b60562bff78047d0fc1a28428ea5
XYIAN <kylexyiandilbeck@gmail.com>
2025-03-12 11:06:47 -0700

    spice up home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 "use client";
 
+import { Button } from "primereact/button";
+import "@/styles/home.scss";
+
+const DETAILS = {
+  title: "Welcome to Kyle's Frontend Forge",
+  description: "Crafting sleek, modern, and high-performance web experiences.",
+};
+
 export default function Home() {
+  const handleClick = () => {
+    alert("Button clicked");
+  };
   return (
-    <main className="flex flex-column justify-center items-center h-screen">
-      <div>
-        <p>some basic content</p>
-      </div>
-    </main>
+    <div className="home-container">
+      <h1 className="hero-title">{DETAILS.title}</h1>
+      <p className="hero-subtitle">{DETAILS.description}.</p>
+      <Button
+        label="View My Work"
+        icon="pi pi-arrow-right"
+        className="p-button-lg p-button-primary hero-button"
+        onClick={handleClick}
+      />
+    </div>
   );
 }
