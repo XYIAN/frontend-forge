a70340eec92babda7450c97a6477905f82f5a95d
XYIAN <kylexyiandilbeck@gmail.com>
2025-03-17 20:31:57 -0700

    phantom modal complete

--- src/features/hallOfCreations/PhantomModal.tsx
+++ src/features/hallOfCreations/PhantomModal.tsx
@@ -7,18 +7,31 @@ import "@/styles/_phantomModal.scss";
 
 export const PhantomModal = () => {
   const [visible, setVisible] = useState(false);
+  const [summoning, setSummoning] = useState(false);
+
+  const handleSummon = () => {
+    setSummoning(true);
+    setTimeout(() => {
+      setSummoning(false);
+      setVisible(true);
+    }, 1000); // 1-second chaotic effect before modal appears
+  };
+
+  const handleDismiss = () => {
+    setVisible(false);
+  };
 
   return (
-    <div className="phantom-modal-container">
+    <div className={`phantom-modal-container ${summoning ? "summoning" : ""}`}>
       <Button
         label="Summon the Phantom"
         className="p-button-rounded p-button-outlined summon-btn"
-        onClick={() => setVisible(true)}
+        onClick={handleSummon}
       />
 
       <Dialog
         visible={visible}
-        onHide={() => setVisible(false)}
+        onHide={handleDismiss}
         className="phantom-modal"
         dismissableMask
         draggable={false}
@@ -29,7 +42,7 @@ export const PhantomModal = () => {
           <Button
             label="Banish the Phantom"
             className="p-button-rounded close-btn"
-            onClick={() => setVisible(false)}
+            onClick={handleDismiss}
           />
         </div>
       </Dialog>
